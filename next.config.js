ad9febb92bd5f9f053edb6c4b0ad3e02fdaed810
adelbeit <adelbeit@gmail.com>
2022-07-14 13:28:40 -0700

    setup redirect homepage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/Keys",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
