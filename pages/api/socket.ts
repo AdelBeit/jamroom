8f892cd050cd45018b540a910e97e1a03f6a04f7
adelbeit <adelbeit@gmail.com>
2022-07-14 13:25:03 -0700

    disabled socket

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,22 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Server, Socket } from "socket.io";
 
-const SocketHandler = (req: NextApiRequest, res: NextApiResponse) => {
-  if (res!.socket!.server.io) {
-    console.log("Socket is already running");
-  } else {
-    console.log("Socket is initializing");
-    const io = new Server(res.socket.server);
-    res.socket.server.io = io;
+// const SocketHandler = (req: NextApiRequest, res: NextApiResponse) => {
+//   if (res!.socket!.server.io) {
+//     console.log("Socket is already running");
+//   } else {
+//     console.log("Socket is initializing");
+//     const io = new Server(res.socket.server);
+//     res.socket.server.io = io;
 
-    io.on("connection", (socket: Socket) => {
-      socket.on("input-change", (msg: string) => {
-        socket.broadcast.emit("update-input", msg);
-      });
-    });
-  }
+//     io.on("connection", (socket: Socket) => {
+//       socket.on("input-change", (msg: string) => {
+//         socket.broadcast.emit("update-input", msg);
+//       });
+//     });
+//   }
 
-  res.end();
-};
+//   res.end();
+// };
 
-export default SocketHandler;
+// export default SocketHandler;
+export default {};
