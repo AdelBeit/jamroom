1c9388e0d00c560ca42d3f26b2816e4cbc53954b
BreadCrumb <adelbeit@gmail.com>
2022-08-17 21:29:02 -0700

    fix client side socket connection issue

--- src/utils/socketClient.ts
+++ src/utils/socketClient.ts
@@ -1,21 +1,20 @@
 import { io } from 'socket.io-client';
 
-// client side
+(async function () {
+   if (typeof window !== 'undefined')
+      await fetch(window.location.origin + "/api/socket");
+})();
 export const socket = io({ autoConnect: false });
 
 // when ready to connect
 export const connectSocket = (userID, roomID) => {
    socket.auth = { userID, roomID };
    socket.connect();
-   // initi socket 
    socket.on('connect_error', (err) => {
       console.error(err.message);
    });
 }
 
-
-//client side cleanup
 export const socketCleanup = () => {
    socket.removeAllListeners().disconnect();
 }
-
