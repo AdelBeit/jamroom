1effbe41950d8e8ea611eb0ed0e561c3819db361
Adele <adelbeit@gmail.com>
2022-10-16 20:54:17 -0700

    rewrite _offline.tsx

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -1,13 +1,2 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { PlayersContextProvider } from "../src/utils/PlayersContext";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <PlayersContextProvider>
-      <Component {...pageProps} />
-    </PlayersContextProvider>
-  );
-}
-
-export default MyApp;
+import Page from "./home";
+export default Page;
