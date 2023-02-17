a484df0a54819928b64c0d9ea9bfcf9558105b43
Adele <adelbeit@gmail.com>
2023-02-16 23:14:39 -0800

    home => index.tsx, -themes from css color vars, +lobby screen

---
+++ src/screens/Lobby.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Lobby() {
+  const appTitle = "Jamroom";
+  return (
+    <div>
+      <div className="app_title">
+        <span>{appTitle}</span>
+      </div>
+      <style jsx>{``}</style>
+    </div>
+  );
+}
