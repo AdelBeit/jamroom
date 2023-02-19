e539bde3dabdf8c110eeedd87cc1b21187929835
adel <adelbeit@gmail.com>
2023-02-19 12:25:29 -0800

    +_jammers page

---
+++ src/screens/Jammers.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Icon from "../components/Icon";
+import JammerBar from "../components/JammerBar";
+
+export default function Jammers() {
+  const users = ["pumpkinman823", "gnarlybarley442", "icebucket889"];
+
+  return (
+    <div id="_Jammers" className="_page flex">
+      {users.map((user) => {
+        let classes = "";
+        if (user == "pumpkinman812") classes = "active";
+        return (
+          <JammerBar
+            setVolume={() => {}}
+            userID={user}
+            volume={-15}
+            classes="active"
+          />
+        );
+      })}
+      <button className={`add_jammer bar flex`}>
+        <Icon _name="plus" size={25} />
+      </button>
+      <style jsx>{``}</style>
+    </div>
+  );
+}
