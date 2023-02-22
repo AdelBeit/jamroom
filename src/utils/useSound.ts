630b0689951782ef7678e8db94f28165f50f848a
adel <adelbeit@gmail.com>
2023-02-22 01:07:00 -0800

    rework setsample to pull padid from state

--- src/utils/useSound.ts
+++ src/utils/useSound.ts
@@ -1,19 +1,22 @@
 import create from "zustand";
 import { devtools } from "zustand/middleware";
-import { Sample } from "../samples";
+import { Sample } from "../sample";
 import { Octave } from "../types";
 
 export type samples = { [name: string]: string };
 export interface SoundStateStore {
   octave: Octave;
+  configPad: number,
   samples: Sample[];
-  setPadSample(padID: number, sample: Sample): void;
+  setPadSample(sample: Sample): void;
+  setConfigPad(padID: number): void;
   setOctave(octave: Octave): void;
 }
 
 export const useSound = create<SoundStateStore>()(
   devtools((set) => ({
     octave: 4,
+    configPad: 1,
     samples: [
       "House Toms",
       "House Toms",
@@ -26,12 +29,13 @@ export const useSound = create<SoundStateStore>()(
       "House Toms",
       "House Toms",
     ],
-    setPadSample: (padID, sample) =>
+    setPadSample: (sample) =>
       set((state) => {
         const samples = state.samples;
-        samples[padID] = sample;
+        samples[state.configPad] = sample;
         return { ...{ samples } };
       }),
+    setConfigPad: (padID) => set({ configPad: padID }),
     setOctave: (octave) => set({ octave }),
   }))
 );
