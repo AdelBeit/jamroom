5b0aac7131c0099a250f5d9bb231ecc16e65f850
Adele <adelbeit@gmail.com>
2023-02-23 00:19:20 -0800

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,36 @@
-import { useScreenStore } from "../src/utils/stores";
 import { NextPage } from "next";
 import { usePlayers } from "../src/utils/PlayersContext";
 import { getSamples } from "../src/utils/data/getSampleNames";
 import { useEffect } from "react";
 import { Lobby } from "../src/screens/Lobby";
 import PageFrame from "../src/screens/PageFrame";
-import { Page as PageType } from "../src/types";
 import Jammers from "../src/screens/Jammers";
+import Drumkit from "../src/screens/Drumkit";
+import Keyboard from "../src/screens/Keyboard";
+import Samples from "../src/screens/Samples";
+import { usePage } from "../src/utils/usePage";
 
 /**
+ * TODO: leave room bug: playerscontext, and handler
+ * TODO: icons
  * optional:
  * TODO: implement room hosts
  * TODO: kick user functionality
  * TODO: change volume slider to be the whole info bar
  * TODO: persist configs
- *
+ * TODO: animate lobby button transitions
  */
 
 const Page: NextPage = (props) => {
-  const screen = useScreenStore((state) => state.selectedScreen);
   const { setSamples } = usePlayers();
-  const _page: PageType = "_Lobby";
+  const _page = usePage((state) => state.page);
   let pageComponent = <Lobby />;
-  // if (_page === "_Jammers") pageComponent = <Jammers />;
-  // if (_page === "_Drumkit" || _page === "_Config") pageComponent = <Drumkit />;
-  // if (_page === "_Keyboard") pageComponent = <Keyboard />;
-  // if (_page === "_Samples") pageComponent = <SoundClips />;
+
+  if (_page === "_Jammers") pageComponent = <Jammers />;
+  if (["_Drumkit", "_Config"].includes(_page))
+    pageComponent = <Drumkit {...{ _page }} />;
+  if (_page === "_Keyboard") pageComponent = <Keyboard />;
+  if (_page === "_Samples") pageComponent = <Samples />;
 
   useEffect(() => {
     // @ts-ignore
