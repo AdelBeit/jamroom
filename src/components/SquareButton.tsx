d86989db4ae9cbf9028855f8d36491c65570daef
Adele Beitvashahi <adelbeit@gmail.com>
2026-03-22 01:38:32 -0600

    Avoid touch/mouse double-fire on SquareButton

--- src/components/SquareButton.tsx
+++ src/components/SquareButton.tsx
@@ -6,15 +6,29 @@ import { preventDefault } from "../utils/preventDefault";
 interface Props extends Omit<ButtonProps, "text"> {}
 
 export default function SquareButton({ _icon, handler, size = 30 }: Props) {
+  const isCoarse =
+    typeof window !== "undefined" &&
+    window.matchMedia("(pointer: coarse)").matches;
+
   return (
     <button
-      onMouseDown={handler}
-      onTouchStart={(e) => {
-        e.preventDefault();
-        handler(e);
-      }}
-      onTouchEnd={preventDefault}
-      onMouseUp={preventDefault}
+      onMouseDown={
+        isCoarse
+          ? undefined
+          : (e) => {
+              handler(e);
+            }
+      }
+      onTouchStart={
+        isCoarse
+          ? (e) => {
+              e.preventDefault();
+              handler(e);
+            }
+          : undefined
+      }
+      onTouchEnd={isCoarse ? preventDefault : undefined}
+      onMouseUp={isCoarse ? undefined : preventDefault}
       className={`mold ${_icon}`}
     >
       <Icon {...{ _icon }} size={17} />
