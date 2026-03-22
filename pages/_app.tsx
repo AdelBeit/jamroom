764775322cd95f163170240f5c47a2f9d7fbeb8c
Adele Beitvashahi <adelbeit@gmail.com>
2026-03-22 01:11:09 -0600

    Refine rotate modal overlay

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,26 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { PlayersContextProvider } from "../src/utils/PlayersContext";
 import Icon from "../src/components/Icon";
+import { usePage } from "../src/hooks/usePage";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const page = usePage((state) => state.page);
+  const showRotateNotice = page !== "_Loading";
+
   return (
     <PlayersContextProvider>
-      <div id="rotate-notice">
-        <div className="rotate-row">
-          <Icon _icon="rotateDevice" size={60} />
-          <p>Please rotate your device.</p>
+      {showRotateNotice && (
+        <div id="rotate-notice">
+          <div className="dark_underlay faded absolute"></div>
+          <div className="box mold">
+            <div className="rotate-row">
+              <Icon _icon="rotateDevice" size={60} />
+              <p>Please rotate your device.</p>
+            </div>
+            <p>This app works best in landscape mode on mobile devices.</p>
+          </div>
         </div>
-        <p>This app works best in landscape mode on mobile devices.</p>
-      </div>
+      )}
       <Component {...pageProps} />
     </PlayersContextProvider >
   );
